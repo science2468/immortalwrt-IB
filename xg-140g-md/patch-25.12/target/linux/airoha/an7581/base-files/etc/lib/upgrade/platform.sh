6d58d63ab31ca6ca32a92dffaaea0b8a1f04a1e8
山茱萸 <102643084+science2468@users.noreply.github.com>
2026-05-21 18:19:27 +0800

    测试增加的固件升级脚本

---
+++ xg-140g-md/patch-25.12/target/linux/airoha/an7581/base-files/etc/lib/upgrade/platform.sh
@@ -0,0 +1,23 @@
+REQUIRE_IMAGE_METADATA=1
+RAMFS_COPY_BIN='fitblk fit_check_sign'
+
+platform_do_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+		gemtek,w1700k-ubi)
+			fit_do_upgrade "$1"
+			;;
+        bell_xg-040g-md)
+            CI_UBIPART="ubi"
+            nand_do_upgrade "$1"
+            ;;
+		*)
+			nand_do_upgrade "$1"
+			;;
+	esac
+}
+
+platform_check_image() {
+	return 0
+}
